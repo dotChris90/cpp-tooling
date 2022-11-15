23de5566b4ddaf8372d329db97ad224c46f41915
dotChris90 <Christian.Kahr@gmx.de>
2022-11-15 05:26:57 -0500

    added cppcheck methods

--- src/tools/cppcheck.ts
+++ src/tools/cppcheck.ts
@@ -2,6 +2,8 @@
 /* eslint-disable unicorn/prefer-module */
 /* eslint-disable promise/catch-or-return */
 /* eslint-disable unicorn/import-style */
+import * as path from 'path';
+
 import { Executor } from '../executor';
 
 export class CppCheck {
@@ -16,5 +18,61 @@ export class CppCheck {
         
         this.exec = exec;
         this.cppcheckBin = cppcheckBin;
+    }
+
+    public generateReportXML(
+        sourceDir : string,
+        reportFile : string) : Promise<void> {
+        const cmd = this.cppcheckBin;
+        const args = [
+            "--force",
+            "--enable=all",
+            "--xml",
+            "--xml-version=2",
+            `--output-file=${reportFile}`,
+            `${sourceDir}`
+        ];
+        const workDir = path.dirname(sourceDir);
+        return this.exec.execAsync(cmd, args, workDir);
+    }
+
+    public convertXML2HTml(
+        sourceDir : string,
+        xmlReportFile : string,
+        outDst : string) : Promise<void> {
+        //"cppcheck-htmlreport --source-dir=. --title=VIMs_SRC --file=report-src.xml --report-dir=VIMs_SRC"
+        const cmd = `${this.cppcheckBin}-htmlreport`;
+        const args = [
+            `--source-dir=${sourceDir}`,
+            `--file=${xmlReportFile}`,
+            `--report-dir=${outDst}`
+        ];
+        const workDir = path.dirname(sourceDir);
+        return this.exec.execAsync(cmd, args, workDir);
+    }
+
+    public generateReportText(
+        sourceDir : string,
+        reportFile : string) : Promise<void> {
+        const cmd = this.cppcheckBin;
+        const args = [
+            "--force",
+            "--enable=all",
+            `--output-file=${reportFile}`,
+            `${sourceDir}`
+        ];
+        const workDir = path.dirname(sourceDir);
+        return this.exec.execAsync(cmd, args, workDir);
+    }
+
+    public srcDirHasError(
+        sourceDir : string) : boolean {
+        const cmd = this.cppcheckBin;
+        const args = [
+            "--force",
+            `${sourceDir}`
+        ];
+        const buffer = this.exec.execSyncGetBuffer(cmd,args);
+        return buffer.stderr.toString() !== "";
     }
 }
